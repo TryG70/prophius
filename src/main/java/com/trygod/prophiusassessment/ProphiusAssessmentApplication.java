89a54838eee25f88aaff23b77189daff9776b599
TryGod <trygodnwakwasi@gmail.com>
2023-08-03 23:48:10 +0100

    initial commit

---
+++ src/main/java/com/trygod/prophiusassessment/ProphiusAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.trygod.prophiusassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProphiusAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProphiusAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trygod/prophiusassessment/ProphiusAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trygod.prophiusassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProphiusAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
